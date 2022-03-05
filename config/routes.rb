0ac9fde157a88167c111aaa4c3ff83f170ec42c4
Vivian Yim <keroviv86@gmail.com>
2022-03-05 15:14:09 -0600

    model, sessions, users, done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   
   resources :user_join_films
   resources :films
-  resources :users
+  resources :users, only: [:index, :create]
+  resources :sessions
+
+
+  # post "/login", to: "sessions#login"
+  # destroy "/logout", to: "sessions#logout"
+
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
