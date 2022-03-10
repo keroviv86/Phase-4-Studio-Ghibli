17949eb9a07a98f75ee10780aa0fd23ac78780fe
Vivian Yim <keroviv86@gmail.com>
2022-03-10 16:56:51 -0600

    fixed rerending and css

--- app/serializers/film_serializer.rb
+++ app/serializers/film_serializer.rb
@@ -1,9 +1,12 @@
 class FilmSerializer < ActiveModel::Serializer
-  attributes :id, :title, :original_title, :image, :description, :director, :producer, :release_date, :running_time, :rt_score, :people, :species, :reviews
-  
+  attributes :id, :title, :original_title, :image, :description, :director, :producer, :release_date,
+   :running_time, :rt_score, :people, :species, :reviews
+
   def reviews
     object.user_join_films.collect do |review|
       {:id => review.id, :user_name => review.user.name, :comment => review.comment, :rating => review.rating}
     end
   end
 end
+
+
